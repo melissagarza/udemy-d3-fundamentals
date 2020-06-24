d408d7ce50ea7e6b2ceec5a7f879b3e0eba0e39d
Melissa Garza <melissa.garza@utexas.edu>
2020-06-24 14:47:58 -0500

    Add some text manually and with D3

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   d3.select('body')
     .append('svg')
-      .attr('width', 50)
+      .attr('width', 250)
       .attr('height', 50)
-    .append('circle')
-      .attr('cx', 25)
-      .attr('cy', 25)
-      .attr('r', 25)
-      .style('fill', 'purple');
+    .append('text')
+      .text('Easy Peasy')
+      .attr('x', 0)
+      .attr('y', 25)
+      .style('fill', 'blue');
 });
