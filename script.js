23126c3994a407c817db8baec4ab2b917fb89bb3
Melissa Garza <melissa.garza@utexas.edu>
2020-06-24 15:15:30 -0500

    Redo bar chart

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  const w = 200;
-  const h = 100;
+  const width = 300;
+  const height = 100;
   const padding = 2;
-  const dataset = [5, 10, 15, 20, 25];
-  
-  const svg = d3.select('body')
-                .append('svg')
-                  .attr('width', w)
-                  .attr('height', h);
+  const dataset = [5, 10, 14, 20, 25];
 
+  const svg = d3.select('body')
+    .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+  
   svg.selectAll('rect')
     .data(dataset)
     .enter()
     .append('rect')
-      .attr('x', (d, i) => i * (w / dataset.length))
-      .attr('y', (d) => h - d)
-      .attr('width', w / dataset.length - padding)
-      .attr('height', (d) => d);
+      .attr('x', (d, i) => i * (width / dataset.length))
+      .attr('y', d => height - (d * 4))
+      .attr('width', width / dataset.length - padding)
+      .attr('height', d => d * 4);
 });
