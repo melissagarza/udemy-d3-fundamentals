ca4ede79fa63d729d501a442adba40645569cc98
Melissa Garza <melissa.garza@utexas.edu>
2020-06-24 15:58:09 -0500

    Add labels to line chart

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  const width = 200;
-  const height = 100;
+  const width = 400;
+  const height = 350;
 
   const monthlySales = [
-    {'month': 10, 'sales': 20},
-    {'month': 20, 'sales': 14},
-    {'month': 30, 'sales': 20},
-    {'month': 40, 'sales': 21},
-    {'month': 50, 'sales': 15},
-    {'month': 60, 'sales': 22},
-    {'month': 70, 'sales': 9},
-    {'month': 80, 'sales': 6},
-    {'month': 90, 'sales': 23},
-    {'month': 100, 'sales': 7}
+    {'month': 10, 'sales': 100},
+    {'month': 20, 'sales': 130},
+    {'month': 30, 'sales': 250},
+    {'month': 40, 'sales': 300},
+    {'month': 50, 'sales': 265},
+    {'month': 60, 'sales': 225},
+    {'month': 70, 'sales': 180},
+    {'month': 80, 'sales': 120},
+    {'month': 90, 'sales': 145},
+    {'month': 100, 'sales': 130}
   ];
 
   const lineFun = d3.svg.line()
-    .x(d => d.month * 2)
-    .y(d => d.sales)
+    .x(d => d.month * 3)
+    .y(d => height - d.sales)
     .interpolate('linear');
 
   const svg = d3.select('body').append('svg')
@@ -33,4 +33,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
       'stroke-width': 2,
       fill: 'none'
     });
+
+  const labels = svg.selectAll('text')
+    .data(monthlySales)
+    .enter()
+    .append('text')
+      .text(d => d.sales)
+      .attr({
+        x: d => d.month * 3 - 25,
+        y: d => height - d.sales,
+        'font-family': 'sans-serif',
+        'font-size': '12px',
+        fill: '#666666',
+        'text-anchor': 'start',
+        dy: '.35em',
+        'font-weight': (d, i) => {
+          if (i === 0 || i === monthlySales.length - 1) {
+            return 'bold';
+          }
+          return 'normal';
+        }
+      });
 });
