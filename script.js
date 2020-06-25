fdcd23625873751d940a613944510ef2883750de
Melissa Garza <melissa.garza@utexas.edu>
2020-06-25 17:52:43 -0500

    Change color of certain data points based on their value using a helper function

--- script.js
+++ script.js
@@ -15,43 +15,51 @@ document.addEventListener('DOMContentLoaded', (event) => {
     {'month': 100, 'sales': 130}
   ];
 
-  const lineFun = d3.svg.line()
-    .x(d => d.month * 3)
-    .y(d => height - d.sales)
-    .interpolate('linear');
+  const salseKPI = d => {
+    if (d >= 250) {
+      return '#33cc66'
+    }
+    return '#666666';
+  };
+
+  const showMinMax = (ds, col, val, type) => {
+    const max = d3.max(ds, d => d[col]);
+    const min = d3.min(ds, d => d[col]);
+
+    if (type === 'minmax' && (val == max || val == min)) {
+      return val;
+    } else {
+      if (type === 'all') {
+        return val;
+      }
+    }
+  };
 
   const svg = d3.select('body').append('svg')
-    .attr({
-      width,
-      height
-    });
+    .attr({ width, height });
 
-  const viz = svg.append('path')
-    .attr({
-      d: lineFun(monthlySales),
-      stroke: 'purple',
-      'stroke-width': 2,
-      fill: 'none'
-    });
-
+  const dots = svg.selectAll('circle')
+    .data(monthlySales)
+    .enter()
+    .append('circle')
+      .attr({
+        cx: d => d.month * 3,
+        cy: d => height - d.sales,
+        r: 5,
+        fill: d => salseKPI(d.sales)
+      });
+  
   const labels = svg.selectAll('text')
     .data(monthlySales)
     .enter()
     .append('text')
-      .text(d => d.sales)
-      .attr({
-        x: d => d.month * 3 - 25,
-        y: d => height - d.sales,
-        'font-family': 'sans-serif',
-        'font-size': '12px',
-        fill: '#666666',
-        'text-anchor': 'start',
-        dy: '.35em',
-        'font-weight': (d, i) => {
-          if (i === 0 || i === monthlySales.length - 1) {
-            return 'bold';
-          }
-          return 'normal';
-        }
-      });
+      .text(d => showMinMax(monthlySales, 'sales', d.sales, 'minmax'))
+        .attr({
+          x: d => d.month * 3 - 28,
+          y: d => height - d.sales,
+          'font-size': '12px',
+          'font-family': 'sans-serif',
+          fill: '#666666',
+          'text-anchor': 'start'
+        });
 });
