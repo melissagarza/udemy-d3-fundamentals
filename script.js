ef3b919b019d7212c3554db66f22133f0d22229c
Melissa Garza <melissa.garza@utexas.edu>
2020-06-27 18:19:36 -0500

    Change how much of the data is displayed in each line chart based on dropdown selection

--- script.js
+++ script.js
@@ -43,21 +43,60 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     const svg = d3.select('body').append('svg')
       .attr('width', width)
-      .attr('height', height);
+      .attr('height', height)
+      .attr('class', `svg-${(data.category).toLowerCase()}`);
     
     const axisX = svg.append('g').call(genAxisX)
-      .attr('class', 'axis')
+      .attr('class', 'axis-x')
       .attr('transform', `translate(0, ${height - padding})`);
 
     const axisY = svg.append('g').call(genAxisY)
-      .attr('class', 'axis')
+      .attr('class', 'axis-y')
       .attr('transform', `translate(${padding}, 0)`);
 
     const viz = svg.append('path')
       .attr('d', lineFun(data.monthlySales))
+      .attr('class', `path-${(data.category).toLowerCase()}`)
       .attr('stroke', 'purple')
       .attr('stroke-width', 2)
       .attr('fill', 'none');
+  };
+
+  const updateLine = data => {
+    
+    const minDate = getDate(data.monthlySales[0]['month']);
+    const maxDate = getDate(data.monthlySales[data.monthlySales.length - 1]['month']);
+
+    const scaleX = d3.time.scale()
+      .domain([minDate, maxDate])
+      .range([padding, width - padding]);
+
+    const scaleY = d3.scale.linear()
+      .domain([0, d3.max(data.monthlySales, d => d.sales)])
+      .range([height - padding, 10]);
+
+    const genAxisX = d3.svg.axis()
+      .scale(scaleX)
+      .orient('bottom')
+      .tickFormat(d3.time.format('%b'))
+      .ticks(data.monthlySales.length - 1);
+
+    const genAxisY = d3.svg.axis()
+      .scale(scaleY)
+      .orient('left')
+      .ticks(4);
+
+    const lineFun = d3.svg.line()
+      .x(d => scaleX(getDate(d.month)))
+      .y(d => scaleY(d.sales))
+      .interpolate('linear');
+
+    const svg = d3.select('body').select(`.svg-${(data.category).toLowerCase()}`);
+    const axisX = svg.selectAll('g.axis-x').call(genAxisX);
+    const axisY = svg.selectAll('g.axis-y').call(genAxisY);
+
+    const viz = svg.selectAll(`.path-${(data.category).toLowerCase()}`)
+      .attr('d', lineFun(data.monthlySales));
   };
 
   const showHeader = data => {
@@ -69,11 +108,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
     (err, data) => {
       if (err) console.log(err);
 
-      const decodedData = JSON.parse(window.atob(data.content));
+      let decodedData = JSON.parse(window.atob(data.content));
 
       decodedData.contents.forEach(data => {
         buildLine(data);
         showHeader(data);
       });
+
+      d3.select('select')
+        .on('change', (d, i) => {
+          const selection = d3.select('#date-option').node().value;
+
+          decodedData = JSON.parse(window.atob(data.content));
+          decodedData.contents.forEach(data => {
+            data.monthlySales.splice(0, data.monthlySales.length - selection);
+            updateLine(data);
+          });
+        });
     });
 });
