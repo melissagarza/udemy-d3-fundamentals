0be30f636d57d38834bc8d0c904e31aee51c5007
Melissa Garza <melissa.garza@utexas.edu>
2020-06-27 17:11:56 -0500

    Switch back to the scatterplot graph

--- script.js
+++ script.js
@@ -1,79 +1,65 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+  const width = 400;
+  const height = 350;
 
-  const width = 300;
-  const height = 100;
-  const padding = 20;
+  const monthlySales = [
+    {'month': 10, 'sales': 100},
+    {'month': 20, 'sales': 130},
+    {'month': 30, 'sales': 250},
+    {'month': 40, 'sales': 300},
+    {'month': 50, 'sales': 265},
+    {'month': 60, 'sales': 225},
+    {'month': 70, 'sales': 180},
+    {'month': 80, 'sales': 120},
+    {'month': 90, 'sales': 145},
+    {'month': 100, 'sales': 130}
+  ];
 
-  const getDate = d => {
-    const strDate = new String(d);
-    const year = strDate.substr(0, 4);
-    const month = strDate.substr(4, 2) - 1;
-    const day = strDate.substr(6, 2);
-
-    return new Date(year, month, day);
-  };
-  
-  const buildLine = data => {
-    
-    const minDate = getDate(data.monthlySales[0]['month']);
-    const maxDate = getDate(data.monthlySales[data.monthlySales.length - 1]['month']);
-
-    const scaleX = d3.time.scale()
-      .domain([minDate, maxDate])
-      .range([padding, width - padding]);
-
-    const scaleY = d3.scale.linear()
-      .domain([0, d3.max(data.monthlySales, d => d.sales)])
-      .range([height - padding, 10]);
-
-    const genAxisX = d3.svg.axis()
-      .scale(scaleX)
-      .orient('bottom')
-      .tickFormat(d3.time.format('%b'));
-
-    const genAxisY = d3.svg.axis()
-      .scale(scaleY)
-      .orient('left')
-      .ticks(4);
-
-    const lineFun = d3.svg.line()
-      .x(d => scaleX(getDate(d.month)))
-      .y(d => scaleY(d.sales))
-      .interpolate('linear');
-
-    const svg = d3.select('body').append('svg')
-      .attr('width', width)
-      .attr('height', height);
-    
-    const axisX = svg.append('g').call(genAxisX)
-      .attr('class', 'axis')
-      .attr('transform', `translate(0, ${height - padding})`);
-
-    const axisY = svg.append('g').call(genAxisY)
-      .attr('class', 'axis')
-      .attr('transform', `translate(${padding}, 0)`);
-
-    const viz = svg.append('path')
-      .attr('d', lineFun(data.monthlySales))
-      .attr('stroke', 'purple')
-      .attr('stroke-width', 2)
-      .attr('fill', 'none');
+  const salseKPI = d => {
+    if (d >= 250) {
+      return '#33cc66'
+    }
+    return '#666666';
   };
 
-  const showHeader = data => {
-    d3.select('body').append('h1')
-      .text(`${data.category} Sales (2013)`);
+  const showMinMax = (ds, col, val, type) => {
+    const max = d3.max(ds, d => d[col]);
+    const min = d3.min(ds, d => d[col]);
+
+    if (type === 'minmax' && (val == max || val == min)) {
+      return val;
+    } else {
+      if (type === 'all') {
+        return val;
+      }
+    }
   };
 
-  d3.json('https://api.github.com/repos/bsullins/d3js-resources/contents/monthlySalesbyCategoryMultiple.json',
-    (err, data) => {
-      if (err) console.log(err);
+  const svg = d3.select('body').append('svg')
+    .attr({ width, height });
 
-      const decodedData = JSON.parse(window.atob(data.content));
-
-      decodedData.contents.forEach(data => {
-        buildLine(data);
-        showHeader(data);
+  const dots = svg.selectAll('circle')
+    .data(monthlySales)
+    .enter()
+    .append('circle')
+      .attr({
+        cx: d => d.month * 3,
+        cy: d => height - d.sales,
+        r: 5,
+        fill: d => salseKPI(d.sales)
       });
-    });
+
+  const labels = svg.selectAll('text')
+    .data(monthlySales)
+    .enter()
+    .append('text')
+      .text(d => showMinMax(monthlySales, 'sales', d.sales, 'minmax'))
+        .attr({
+          x: d => d.month * 3 - 28,
+          y: d => height - d.sales,
+          'font-size': '12px',
+          'font-family': 'sans-serif',
+          fill: '#666666',
+          'text-anchor': 'start'
+        });
 });
