be353fc53d99c4878a9860e58da98dc1b0847759
Melissa Garza <melissa.garza@utexas.edu>
2020-06-29 15:18:30 -0500

    Draw the GeoJSON data of the United States

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  
+  const width = 500;
+  const height = 300;
+
+  const projection = d3.geo.albersUsa()
+    .translate([width / 2, height / 2])
+    .scale([500]);
+
+  const path = d3.geo.path().projection(projection);
+
+  const svg = d3.select('body').append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+  d3.json('us.json', json => {
+    svg.selectAll('path')
+      .data(json.features)
+      .enter()
+      .append('path')
+        .attr('d', path)
+        .attr('fill', '#666666');
+  });
 });
