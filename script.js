99e4fd26d0d319de666b28f8725dd4554b700bdd
Melissa Garza <melissa.garza@utexas.edu>
2020-06-29 16:11:24 -0500

    Added dots to the map and made a size that correlates to the data

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  const width = 800;
-  const height = 500;
+  const width = 1200;
+  const height = 800;
 
   const projection = d3.geo.albersUsa()
     .translate([width / 2, height / 2])
@@ -19,6 +19,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
       .append('path')
         .attr('d', path)
         .style('fill', '#666666');
+
+      d3.csv('sales-by-city.csv', data => {
+        svg.selectAll('circle')
+          .data(data)
+          .enter()
+          .append('circle')
+            .attr('cx', d => {
+              if (d.lon !== '#N/A') {
+                return projection([d.lon, d.lat])[0];
+              }
+              return 0;
+            })
+            .attr('cy', d => {
+              if (d.lat !== '#N/A') {
+                return projection([d.lon, d.lat])[1];
+              }
+              return 0;
+            })
+            .attr('r', d => Math.sqrt(parseInt(d.sales) * 0.0005))
+            .style('fill', 'red');
+      });
   });
 
 });
